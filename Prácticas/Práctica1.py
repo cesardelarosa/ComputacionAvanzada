f2125a06a0611e918a830b0b39ebc96a59f03761
César de la Rosa <cesardelarosa.code@proton.me>
2024-01-31 16:53:43 +0100

    nuevo orden

--- Prácticas/Práctica1.py
+++ Prácticas/Práctica1.py
@@ -14,15 +14,19 @@ Repositorio web:
 Computación Avanzada, Física UAM.
 '''
 import math
+import sys
 
 g = 9.81
 
 h = float(input("¿Cúal es la altura de la torre (en metros)? "))
 t = float(input("¿Para qué tiempo quieres saber la altura de la bola (en segundos)? "))
+if h < 0 or t < 0:
+	print("No se aceptan valores negativos.")
+	sys.exit()
 
-y = h - g * t * t
+y = h - g * t**2 /2
 if y > 0:
-    print("La altura de la bola tras", t," segundos es de ", y," metros.")
+	print(f"La altura de la bola tras {t} segundos es de {y:.3f} metros.")
 else:
-    tf = math.sqrt(h/g)
-    print("La bola ya llegó al suelo en el segundo", tf)
+	tf = math.sqrt(2 * h / g)
+	print(f"La bola ya llegó al suelo en el segundo {tf:.3f}")
