d3a26855c5a9bbc4f2c5bc29a0ce44cfa140f0a0
César de la Rosa <cesardelarosa.code@proton.me>
2024-01-31 16:25:00 +0100

    prueba en  clase

--- Prácticas/tests/test1.py
+++ Prácticas/tests/test1.py
@@ -1,8 +1,28 @@
+'''
+Práctica inicial
 
-from vpython import sphere, vector, rate
+Problema:
+    Una bola se deja caer desde una torre.
+    Calcular la altura de la bola en un tiempo t (input)
 
-my_sphere = sphere(pos=vector(0, 0, 0), radius=0.5)
+Alumno:
+    César de la Rosa (cesar.rosa@estudiante.uam.es)
 
-while True:
-	rate(60)
-	my_sphere.pos.x += 0.01
+Repositorio web:
+    https://github.com/cesardelarosa/ComputacionAvanzada
+    
+Computación Avanzada, Física UAM.
+'''
+import math
+
+g = 9.81
+
+h = float(input("¿Cúal es la altura de la torre (en metros)? "))
+t = float(input("¿Para qué tiempo quieres saber la altura de la bola (en segundos)? "))
+
+y = h - g * t * t
+if y > 0:
+    print("La altura de la bola tras", t," segundos es de ", y," metros.")
+else:
+    tf = math.sqrt(h/g)
+    print("La bola ya llegó al suelo en el segundo", tf)
