9d86284af7f458c66a8fe984fadc4b244964b6a4
César de la Rosa <cesardelarosa.code@proton.me>
2024-02-06 11:51:24 +0100

    newman 2.1

--- Prácticas/Newman_2-1.py
+++ Prácticas/Newman_2-1.py
@@ -13,8 +13,8 @@ Repositorio web:
     
 Computación Avanzada, Física UAM.
 '''
-import math
-import sys
+from math import sqrt
+from sys import exit
 
 g = 9.81
 
@@ -22,11 +22,11 @@ h = float(input("¿Cúal es la altura de la torre (en metros)? "))
 t = float(input("¿Para qué tiempo quieres saber la altura de la bola (en segundos)? "))
 if h < 0 or t < 0:
 	print("No se aceptan valores negativos.")
-	sys.exit()
+	exit()
 
 y = h - g * t**2 /2
 if y > 0:
 	print(f"La altura de la bola tras {t} segundos es de {y:.3f} metros.")
 else:
-	tf = math.sqrt(2 * h / g)
+	tf = sqrt(2 * h / g)
 	print(f"La bola ya llegó al suelo en el segundo {tf:.3f}")
