bf8ef4dffc8d6dd213c06f3c1a06ca08ab658d7b
César de la Rosa <cesardelarosa.code@proton.me>
2024-02-07 15:28:20 +0100

    Actualizar Giordano_1-1.py

--- Prácticas/Giordano_1-1.py
+++ Prácticas/Giordano_1-1.py
@@ -1,3 +1,19 @@
+
+'''
+Práctica inicial
+
+Problema:
+    Calcular la velocidad de un cuerpo por acción de la gravedad comparando la solución analítica con el método de Euler
+
+Alumno:
+    César de la Rosa (cesar.rosa@estudiante.uam.es)
+
+Repositorio web:
+    https://github.com/cesardelarosa/ComputacionAvanzada
+    
+Computación Avanzada, Física UAM.
+'''
+
 g = 9.8
 t_max = 10
 h = float(input("Defina el intervalo de tiempo 'h' en segundos"))
